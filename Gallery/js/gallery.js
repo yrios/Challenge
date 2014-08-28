44419a6982773f4e4a4e94a70329d9c69ab89abf
yrios <yrios@nativapps.com>
2014-08-27 20:13:22 -0500

    Done

--- Gallery/js/gallery.js
+++ Gallery/js/gallery.js
@@ -9,6 +9,7 @@ function Gallery (gallery,_data,_mode) {
 	this.data = _data;  // data field is an img array that looks like this [{src:"imgsrc",desc:"img desc"},{src:"imgsrc",desc:"img desc"}]
 	this.mode = _mode;  //Specify the mode of the Widget (gallery/single);
 	this.currentImg = 0;
+	this.selected = null;
 	this.numberSlides = 0;
 	this.navslide = null;
 }
@@ -20,11 +21,11 @@ function Gallery (gallery,_data,_mode) {
 Gallery.prototype = {
 	init : function(){
 		if(this.mode == "single"){
-			this.render();
-			this.navigation();
+			this.renderSingle();
+			this.navigationSingle();
 		}else if(this.mode == "gallery"){
-			this.render();
-			this.navigation();
+			this.renderGallery();
+			this.navigationGallery();
 		}else{
 			console.log("invalid mode");
 		}
@@ -40,11 +41,16 @@ Gallery.prototype = {
 		this.display();
 	},
 
+	select: function(x){
+		this.currentImg = x;
+		this.display();
+	},
+
 	/*
-	* Function navigate: this function add the event listener for the slider, this functions 
-	*  calls slide every time that the user clicks on the main area of the slider
+	* Function navigate in single mode: this function add the event listener for the slider, this functions 
+	*  calls "slide" every time that the user clicks on the main area of the slider
 	*/
-	navigation: function(){
+	navigationSingle: function(){
 		var self = this;
 		this.navslide = this.find();
 		this.navslide.addEventListener('click',function(){
@@ -55,17 +61,61 @@ Gallery.prototype = {
 	},
 
 	/*
+	*Function navigate in gallery mode: this function add the event listener to the
+	* thumnails container, and calls "select" every time the user clicks on any of the thumbnails presented
+	*/
+	navigationGallery: function(){
+		var self = this;
+		this.navslide = this.find();
+		this.thumbnails = this.navslide.lastChild;
+		this.thumbnails.addEventListener('click', function(e){
+			this.selected = e.target.id;
+			self.select(this.selected)
+		});
+	},
+
+	/*
 	* Function render: this function renders all the elements on the specified id.
 	*/
-	render: function(){
+	renderSingle: function(){
 		var div = document.createElement("div");
-		div.setAttribute("width","400px");
 		div.setAttribute("class","slider");
 
 		var img = document.createElement("img");
 		img.setAttribute("src", this.data[0].src);
 		div.appendChild(img);
-		this.gallery.appendChild(div);
+		this.gallery.appendChild(div); // set to the main gallery container
+	}, 
+
+	renderGallery: function(){
+		//Slider div
+		var div = document.createElement("div");
+		div.setAttribute("class","slider");
+
+		//----main-slide
+		var img = document.createElement("img");
+		img.setAttribute("src", this.data[0].src);
+		//--end main slide
+
+		//---scrollable div
+		var divscrollable = document.createElement("div");
+		divscrollable.setAttribute("class","scrollable-tumbnail");
+
+		for (var i = 0; i < this.data.length; i++) {
+			var thumbnail = document.createElement("div");
+			thumbnail.setAttribute("id",i);
+			var imgthumbnail = document.createElement("img");
+			imgthumbnail.setAttribute("src", this.data[i].src);
+			imgthumbnail.setAttribute("id",i);
+			thumbnail.appendChild(imgthumbnail);
+			divscrollable.appendChild(thumbnail);
+		};
+		//---End scrollable div
+
+		div.appendChild(img);
+		div.appendChild(divscrollable);
+
+		this.gallery.appendChild(div); // set to the main gallery container
 	},
 
 	/*
